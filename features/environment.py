fdda3f943103d1377a92c49cd65897e13f73575e
inga-ksk <inga.alyakskina@gmail.com>
2022-09-24 16:19:38 -0400

    Firefox and headless are added

--- features/environment.py
+++ features/environment.py
@@ -1,14 +1,53 @@
-from selenium import webdriver
+#import allure
+#from allure_commons.types import AttachmentType
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
+from selenium import webdriver
+# from selenium.webdriver.support.events import EventFiringWebDriver
+# from support.logger import logger, MyListener
+
+# Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+#bs_user = ''
+#bs_key = ''
+
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
 
 def browser_init(context):
     """
     :param context: Behave context
     """
-    context.driver = webdriver.Chrome(executable_path="/Users/Inga/Documents/Careerist/Automation/gettop_intern/chromedriver.exe")
-    # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+    #context.driver = webdriver.Chrome(executable_path="/Users/Inga/Documents/Careerist/Automation/gettop_intern/chromedriver.exe")
+    # context.driver = webdriver.Safari()
+    #context.driver = webdriver.Firefox(executable_path="/Users/Inga/Documents/Careerist/Automation/gettop_intern/geckodriver.exe")
+
+    ## HEADLESS MODE ####
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('window-size=1920x1080');
+    context.driver = webdriver.Chrome(chrome_options=options)
+
+    # Mobile - run tests on mobile web browser
+    # options = webdriver.ChromeOptions()
+    # mobile_emulation = {"deviceName": "Nexus 5"}
+    # options.add_experimental_option("mobileEmulation", mobile_emulation)
+    # context.driver = webdriver.Chrome(chrome_options=options)
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(), MyListener())
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    # for browerstack ###
+    # desired_cap = {
+    #     'browser': 'Firefox',
+    #     'os_version': '11',
+    #     'os': 'Windows',
+    #     'name': test_name
+    # }
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
--- pages/catalog_page.py
+++ pages/catalog_page.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import Page
 from selenium.webdriver.common.action_chains import ActionChains
+import time
+
 
 class CatalogPage(Page):
 
